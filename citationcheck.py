5a3707f3ece751371b58070183e59789fabea670
BenSt099 <ben.steinhauer.de@gmail.com>
2024-09-10 18:23:25 +0200

    updated application; fixed issues

--- citationcheck.py
+++ citationcheck.py
@@ -2,7 +2,7 @@ import os
 import requests
 import customtkinter
 from tkinter import filedialog
-
+from PIL import Image
 
 def contact_crossref_api(email_address, data_dict):
 
@@ -43,13 +43,14 @@ class UpperFrame(customtkinter.CTkFrame):
         if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
             self.toplevel_window = SingleCTk(self) 
             self.toplevel_window.attributes('-topmost', 1)
-    
+            self.toplevel_window.grab_set()
+            
             while self.toplevel_window.closed != True:
-                 self.update()
+                self.update()
 
             k = self.toplevel_window.get_information()  
             
-            self.toplevel_window.close()
+            self.toplevel_window.destroy_all()
             
         else:
             self.toplevel_window.focus()
@@ -60,7 +61,8 @@ class SingleCTk(customtkinter.CTkToplevel):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.geometry("900x220")
-        self.iconbitmap('logo.ico')
+        self.after(201, lambda: self.iconbitmap('logo.ico')) # fix from StackOverflow
+        # https://stackoverflow.com/questions/75825190/how-to-put-iconbitmap-on-a-customtkinter-toplevel
         self.resizable(False, False)
         self.title("CheckCitation - Input")
         self.grid_rowconfigure(3) 
@@ -68,14 +70,15 @@ class SingleCTk(customtkinter.CTkToplevel):
 
         ###############################################################################
         ### Title ###
-        self.label_title = customtkinter.CTkLabel(self, text="Title", font=customtkinter.CTkFont(family='times new roman 14 bold', size=17, weight="bold"))
+        image_questionmark = customtkinter.CTkImage(Image.open("icon_questionmark.png"), size=(16,16))
+        self.label_title = customtkinter.CTkButton(self, fg_color = "transparent", hover_color="#181818", text="Title", image=image_questionmark, compound="right", font=customtkinter.CTkFont(family='times new roman 14 bold', size=17, weight="bold"))
         self.label_title.grid(row=0, column=0, padx=20, pady=20)
 
         self.title_input = customtkinter.CTkEntry(self, placeholder_text="Title", width=500, height=35)
         self.title_input.grid(row=0, column=1, padx=20, pady=20)
         ###############################################################################
         ### DOI ###
-        self.label_doi = customtkinter.CTkLabel(self, text="DOI", font=customtkinter.CTkFont(family='times new roman 14 bold', size=17, weight="bold"))
+        self.label_doi = customtkinter.CTkButton(self, fg_color = "transparent", hover_color="#181818", text="DOI", image=image_questionmark, compound="right", font=customtkinter.CTkFont(family='times new roman 14 bold', size=17, weight="bold"))
         self.label_doi.grid(row=1, column=0, padx=20, pady=20)
 
         self.doi_input = customtkinter.CTkEntry(self, placeholder_text="DOI", width=500, height=35)
@@ -89,6 +92,9 @@ class SingleCTk(customtkinter.CTkToplevel):
         ###############################################################################
 
     def close(self):
+        self.withdraw()
+
+    def destroy_all(self):
         self.destroy()
 
     def collect(self):
@@ -96,9 +102,8 @@ class SingleCTk(customtkinter.CTkToplevel):
             "title_info": self.title_input.get(),
             "doi_info": self.doi_input.get()
         }
-        self.withdraw()
         self.closed = True
-        #self.close()
+        self.withdraw()
 
     def get_information(self):
         return self.information
@@ -113,17 +118,13 @@ class App(customtkinter.CTk):
         self.geometry("800x600")
         self.title("CheckCitation")
         self.iconbitmap('logo.ico')
-        ###self.grid_columnconfigure((2, 1), weight=1)
 
-
-        self.grid_rowconfigure(2, weight=1)  # configure grid system
-        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(2)
+        self.grid_columnconfigure(0)
 
         self.upperframe = UpperFrame(master=self)
         self.upperframe.grid(row=0, column=0, padx=0, pady=0, sticky="ew")
         
-    
-    
-
-app = App()
-app.mainloop()
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
