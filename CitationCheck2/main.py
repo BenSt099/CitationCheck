1d602f0371291a78e4c056914dff625ce2733a5a
BenSt099 <ben.steinhauer.de@gmail.com>
2024-09-25 22:19:06 +0200

    added new version of citationcheck

---
+++ CitationCheck2/main.py
@@ -0,0 +1,23 @@
+import webview
+
+class Api:
+    pass
+    # def addItem(self, title):
+    #     print('Added item %s' % title)
+
+    # def removeItem(self, item):
+    #     print('Removed item %s' % item)
+
+    # def editItem(self, item):
+    #     print('Edited item %s' % item)
+
+    # def toggleItem(self, item):
+    #     print('Toggled item %s' % item)
+
+    # def toggleFullscreen(self):
+    #     webview.windows[0].toggle_fullscreen()
+
+if __name__ == '__main__':
+    api = Api()
+    webview.create_window('CitationCheck', './gui/app.html', js_api=api, text_select=True, maximized=True)
+    webview.start()
